76ec917f2d273a44e996107cb46be86b49fcd0a8
JuanVelezDev <juanes.jevj@gmail.com>
2025-11-19 07:11:27 -0500

    feat: implement timing interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,40 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import { GlobalExceptionFilter } from './common/filters/https-exception.filter'; 
+import { LoggerMiddleware } from './common/middlewares/logger.middleware'; // opcional si ya lo tienes
 
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  const app = await NestFactory.create(AppModule);
 
-
-const config = new DocumentBuilder()
-.setTitle('Task Manager API') 
-.setDescription('API para gestionar tareas')
-.setVersion('1.0')
-.addBearerAuth()
-.build();
-
-
-const doc = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('/api', app, doc);
-
-app.useGlobalPipes(
+  //  Pipes globales para validación y transformación
+  app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // elimina propiedades que no estén en el DTO
-      forbidNonWhitelisted: true, // lanza error si mandan propiedades desconocidas
-      transform: true, // convierte el body al tipo del DTO
+      whitelist: true,              // elimina propiedades que no estén en el DTO
+      forbidNonWhitelisted: true,   // lanza error si mandan propiedades desconocidas
+      transform: true,              // convierte el body al tipo del DTO
     }),
   );
 
+  
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
-await app.listen(process.env.PORT || 3005);
+  //  
+  // app.useGlobalInterceptors(new LoggingInterceptor());
+
+  //  Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Nike Store API') // puedes cambiarlo si quieres
+    .setDescription('API para gestionar usuarios, productos y pedidos')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
+  await app.listen(process.env.PORT || 3007);
+  console.log(`Servidor corriendo en http://localhost:3007/api`);
 }
+
 bootstrap();
