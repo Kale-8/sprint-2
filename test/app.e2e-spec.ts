cef054ff7f2eb1dcd8a4c275fafb74dce061586b
JuanVelezDev <juanes.jevj@gmail.com>
2025-12-04 07:21:13 -0500

    feat: testing and sonarcube

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,69 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// test/auth.e2e-spec.ts
 import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Login', () => {
+    it('POST /auth/login - success', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'admin@nike.com', password: '123456' })
+        .expect(201);
+
+      expect(response.body).toHaveProperty('accessToken');
+      expect(response.body).toHaveProperty('refreshToken');
+    });
+
+    it('POST /auth/login - invalid credentials', async () => {
+      await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'wrong@nike.com', password: '123456' })
+        .expect(401);
+    });
+  });
+
+  describe('Protected route', () => {
+    let accessToken: string;
+
+    beforeAll(async () => {
+      const loginRes = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'admin@nike.com', password: '123456' });
+
+      accessToken = loginRes.body.accessToken;
+    });
+
+    it('GET /auth/protected - success with JWT', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/auth/protected')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200);
+
+      expect(res.body).toHaveProperty('message', 'Ruta protegida');
+      expect(res.body.user).toHaveProperty('email', 'admin@nike.com');
+    });
+
+    it('GET /auth/protected - fail without JWT', async () => {
+      await request(app.getHttpServer())
+        .get('/auth/protected')
+        .expect(401);
+    });
   });
 });
